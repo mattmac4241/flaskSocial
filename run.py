8efd201894be2c2cc9885a50b9f6d8d4ed0144f6
mattmac4241 <mattmac4241@yahoo.com>
2015-12-04 11:51:05 -0600

    production

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from app import manager
 
 
 
-app.run(debug=True)
-'''port = int(os.environ.get('PORT', 5000))
+#app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
-'''
